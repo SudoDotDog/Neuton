66afef1f65b5302aef9d8ddea25e5b3bd99cc8db
WMXPY <wm@mengw.io>
2019-11-09 20:25:31 -0600

    init files

---
+++ src/network.ts
@@ -0,0 +1,17 @@
+/**
+ * @author WMXPY
+ * @namespace Neuton
+ * @description Network
+ */
+
+export class Network {
+
+    public static create() {
+
+        return new Network();
+    }
+
+    private constructor() {
+
+    }
+}
