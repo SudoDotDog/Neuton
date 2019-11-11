750a50b2927c612f936b43fb19308b8ed0f566ae
WMXPY <wm@mengw.io>
2019-11-11 14:22:23 -0600

    add layer basics

--- src/layer.ts
+++ src/layer.ts
@@ -4,6 +4,8 @@
  * @description Layer
  */
 
+import { Neuron } from "./neuron";
+
 export class Layer {
 
     public static create(): Layer {
@@ -11,7 +13,34 @@ export class Layer {
         return new Layer();
     }
 
+    private readonly _neurons: Neuron[];
+    private readonly _requires: Layer[];
+
     private constructor() {
 
+        this._neurons = [];
+        this._requires = [];
+    }
+
+    public addNeuron(neuron: Neuron): this {
+
+        return this.addNeurons(neuron);
+    }
+
+    public addNeurons(...neurons: Neuron[]): this {
+
+        return this.addNeuronList(neurons);
+    }
+
+    public addNeuronList(neuronList: Neuron[]): this {
+
+        this._neurons.push(...neuronList);
+        return this;
+    }
+
+    public requires(...layers: Layer[]): this {
+
+        this._requires.push(...layers);
+        return this;
     }
 }
